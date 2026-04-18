e0d35b5355dc5e9b38db8405af1d82b701cb495b
Samusk26 <samuelamc3@gmail.com>
2026-04-17 19:01:14 -0500

    uno
    se añadio las unciones alfa del uno

--- Bot_ds.py
+++ Bot_ds.py
@@ -2,6 +2,21 @@ import discord
 import random
 from discord.ext import commands
 
+colores = ["verde", "rojo", "azul", "amarillo"]
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0,]
+especiales = ["+2", "reverse", "bloqueo"]
+comodines = ["cambio de color", "+4"]
+mazo = []
+manos = {}
+for color in colores:
+    for numero in numeros:
+        mazo.append((color, numero))
+    for especial in especiales:
+        mazo.append((color, especial))
+for comodin in comodines:
+   mazo.append((None, comodin))
+   
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -18,5 +33,23 @@ async def hello(ctx):
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
+
+@bot.command()
+async def iniciarUNO(ctx):
+    manos.clear()
+    for i in range(7):
+        carta = random.choice(mazo)
+        manos.append(carta)
+        await ctx.send(f"mano: {manos}")
+
+@bot.command()
+async def robar(ctx):
+    usuario = ctx.author.id
+    carta = random.choice(mazo)
+    if carta[0] is None:
+        await ctx.author.send(f'Has robado la carta: {carta[1]}')
+    else:
+        await ctx.author.send(f'Has robado la carta: {carta[0]} {carta[1]}')
+    manos[usuario].append(carta)
 
 bot.run("MTQ4NDcwODY0ODkxMDMyNzg2OA.GC492S.fH1-lloKfVBw410xYVANnj1ftUs1lXsmNyCDUI")
